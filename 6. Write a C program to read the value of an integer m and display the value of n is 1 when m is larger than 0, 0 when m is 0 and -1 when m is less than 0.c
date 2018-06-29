f1ee20c0e32e40863feed636a0ae303780ab7553
Sachin Kumar Kamat <36402247+easykamat@users.noreply.github.com>
2018-06-29 17:10:37 +0530

    Update 6. Write a C program to read the value of an integer m and display the value of n is 1 when m is larger than 0, 0 when m is 0 and -1 when m is less than 0.c

--- 6. Write a C program to read the value of an integer m and display the value of n is 1 when m is larger than 0, 0 when m is 0 and -1 when m is less than 0.c
+++ 6. Write a C program to read the value of an integer m and display the value of n is 1 when m is larger than 0, 0 when m is 0 and -1 when m is less than 0.c
@@ -1,4 +1,4 @@
-/*6. Write a C program to read the value of an integer m and display the value of n is 1 when m is larger than 0, 0 when m is 0 and -1 when m is less than 0. Go to the editor
+/*6. Write a C program to read the value of an integer m and display the value of n is 1 when m is larger than 0, 0 when m is 0 and -1 when m is less than 0.
 Test Data : -5
 Expected Output :
 The value of n = -1*/
