72dbc8c4f387613d78840e49bce6156a7e420525
Sahil Jha <sjha200000@gmail.com>
2018-07-31 21:21:55 +0530

    update this to a c++ program

--- 14.Program in C to read any Month Number in integer and display the number of days for this month.c
+++ 14.Program in C to read any Month Number in integer and display the number of days for this month.c
@@ -1,30 +1,23 @@
-#include<stdio.h>
+#include<iostream>
+using namespace std;
 int main()
 {
-  int num;
-  printf("Input the month in number\n");
-  scanf("%d",&num );
-  switch (num) {
-    case 1:
-    case 3:
-    case 5:
-    case 7:
-    case 8:
-    case 10:
-    case 12:
-      printf("Month have 31 days\n");
-      break;
-    case 2:
-      printf("Month have 28/29 days\n");
-      break;
-    case 4:
-    case 6:
-    case 9:
-    case 11:
-      printf("Month have 30 days\n");
-      break;
-    default:
-      printf("Error..!!Input valid month number\n");
-      break;
+
+  int days[]={30,31};
+  int i,month;
+  cout<<"enter the month in integer\n";
+  cin>>month;
+  if (month == 2)
+  {
+    cout<<"28 days";
   }
+  else if ((month == 1) || (month == 3) || (month == 5) || (month == 7) || (month == 8))
+  {
+    cout<<days[0];
+  }
+  else
+  {
+    cout<<days[1];
+  }
+  return 0;
 }
